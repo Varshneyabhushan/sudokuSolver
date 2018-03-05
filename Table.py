95a9e157a21107c5dd31b97e8e540f143e0b6bc5
varshneyabhushan <tambabathula@iitg.ac.in>
2018-03-05 19:08:59 +0530

    updated iterations counting

--- Table.py
+++ Table.py
@@ -62,14 +62,14 @@ class sudoku:
             if self.expired: #wrong puzzle
                 return {
                     'done' : False,
-                    'iterations' : self.Iterations ,
+                    'iterations' : self.Iterations - 1 ,
                     'guesses' : self.guesses,
                     'timeTaken' : clock(),
                     'solution' : None}
             elif(self.solved == self.__n**2): #solved
                 return {
                     'done' : True ,
-                    'iterations' : self.Iterations ,
+                    'iterations' : self.Iterations - 1 ,
                     'guesses' : self.guesses,
                     'timeTaken' : clock(),
                     'solution' : self.myTable() }
